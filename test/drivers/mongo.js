e09f00ec11d0cdfa80c05ca09b342e7e9cc87e76
thejsj <jorge.silva@thejsj.com>
2015-05-01 21:23:40 -1000

    Add mongo tests and createTables with indexes

--- test/drivers/mongo.js
+++ test/drivers/mongo.js
@@ -1,5 +1,9 @@
 require('should');
+var _ = require('lodash');
+var q = require('q');
+var Promise = require('bluebird');
 var MongoDriver = require('../../lib/drivers/mongodb');
+var mongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
 
 describe('MongoDB', function () {
 
@@ -13,7 +17,7 @@ describe('MongoDB', function () {
       db: 'motoreTest'
     });
     mongo.connect()
-     .then(done);
+     .then(done.bind(null, null));
   });
 
   describe('connecting', function () {
@@ -30,13 +34,7 @@ describe('MongoDB', function () {
           db: 'databaseThatDoesntExist'
         });
         conn.connect()
-         .then(function () {
-           //console.log('DB Connection');
-           //console.log(conn.conn);
-           done();
-         })
          .catch(function (err) {
-           //console.log('Error', err);
            done();
          });
     });
@@ -59,12 +57,60 @@ describe('MongoDB', function () {
 
   describe('getTables', function () {
 
-    before(function () {
-
+    var tables = ['table1', 'helloWorld' + Math.random(), 'anotherTable' + Math.random()];
+    before(function (done) {
+      mongoClient.connectAsync('mongodb://localhost:27017/motoreTest')
+        .then(function (db) {
+          db = Promise.promisifyAll(db);
+          return db.dropDatabaseAsync()
+            .then(function () {
+              return q.all(tables.map(function (table) {
+                return db.createCollectionAsync(table);
+              }));
+            });
+        })
+        .then(mongoClient.closeAsync)
+        .then(done.bind(null, null));
     });
 
-    xit('should get all the tables in the database', function (done) {
-      done();
+    it('should get all the tables in the database as an object with a name property', function (done) {
+      mongo.getTables()
+        .then(function (collections) {
+          _.pluck(collections, 'name').sort().should.eql(tables.sort());
+          done();
+        });
+    });
+
+    // After
+    after(function (done) {
+      mongoClient.connectAsync('mongodb://localhost:27017/motoreTest')
+        .then(function (db) {
+          db = Promise.promisifyAll(db);
+          return db.dropDatabaseAsync();
+        })
+        .then(mongoClient.closeAsync)
+        .then(done.bind(null, null));
     });
   });
+
+  describe('createTables', function () {
+
+    it('should create tables passed to it as an object', function (done) {
+      var tables = [
+        { name: 'hello' },
+        { name: 'hello' + Math.random() },
+        { name: 'hello2' },
+      ];
+      mongo.createTables(tables)
+        .then(function () {
+          return mongo.getTables();
+        })
+        .then(function (_tables) {
+          _.pluck(tables, 'name').sort().should.eql(_.pluck(tables, 'name').sort());
+          done();
+        })
+        .catch(done);
+    });
+  });
+
 });
