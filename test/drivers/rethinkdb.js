0fcead62be0157a1eaa1a1d5e466c1107bca84df
thejsj <jorge.silva@thejsj.com>
2015-05-11 08:50:44 -0700

    Add RethinkDB Driver and tests

--- test/drivers/rethinkdb.js
+++ test/drivers/rethinkdb.js
@@ -1,8 +1,216 @@
 require('should');
+var _ = require('lodash');
+var q = require('q');
+var Promise = require('bluebird');
+var RethinkDBDriver = require('../../lib/drivers/rethinkdb');
+var r = require('rethinkdb');
+
+var testDBName = 'motoreTest';
+var data = [
+  { number: 1 },
+  { number: 2 },
+  { number: 3 }
+];
+
+
+var dropTestDatabase = function (done) {
+  r.connect(this.connectionOpts)
+    .then(function (conn) {
+      r.dbDrop(testDBName).run(this.conn)
+        .then(function () {
+          return this.conn.close();
+        }.bind(this))
+        .then(done.bind(null, null));
+    });
+};
+
+var insertTestData = function (done, data) {
+  r.connect(this.connectionOpts)
+    .then(function (conn) {
+      r.dbDrop(testDBName).run(this.conn)
+        .then(function () {
+          return r.dbCreate(testDBName).run(conn);
+        })
+        .then(function () {
+          return r.tableCreate('table1').run(conn);
+        })
+        .then(function () {
+          return r.table('table1')
+            .insert(data)
+            .run(conn);
+        })
+        .then(function () {
+          return r.table('table1').indexCreate('exampleIndex').run(conn);
+        })
+        .then(function () {
+          return conn.close();
+        });
+    });
+};
 
 describe('RethinkDB', function () {
 
+  var rdb;
+
+  // Connect to Mongo
+  before(function (done) {
+    rdb = new RethinkDBDriver({
+      host: 'localhost',
+      port: 28015,
+      db: 'motoreTest'
+    });
+    rdb.connect()
+     .then(done.bind(null, null));
+  });
+
   describe('connecting', function () {
 
+    it('should have connected propertly', function () {
+      rdb.conn.should.be.a.Object;
+    });
+
+    // TODO: Should it throw an error? It should create it by default
+    xit('should throw an error if the database doesn\'t exists', function (done) {
+      var conn = new RethinkDBDriver({
+          host: 'localhost',
+          port: 28015,
+          db: 'databaseThatDoesntExist'
+        });
+        conn.connect()
+         .catch(function (err) {
+           done();
+         });
+    });
+
+    it('should throw an error if it can\'t connect to the host', function (done) {
+        var conn = new RethinkDBDriver({
+          host: 'localhost',
+          port: 9999,
+          db: 'databaseThatDoesntExist'
+        });
+        conn.connect()
+         .catch(function (err) {
+           err.name.should.equal('RqlDriverError');
+           err.message.indexOf('ECONNREFUSED').should.not.equal(-1);
+           done();
+         });
+    });
+
+  });
+
+  xdescribe('getTables', function () {
+
+    var tables = ['table1', 'helloWorld' + Math.random(), 'anotherTable' + Math.random()];
+    before(function (done) {
+      r.connect(this.connectionOpts)
+        .then(function (conn) {
+          return r.dbDrop(testDBName).run(conn)
+          // ...
+
+        })
+      mongoClient.connectAsync('mongodb://localhost:27017/motoreTest')
+        .then(function (db) {
+          db = Promise.promisifyAll(db);
+          return db.dropDatabaseAsync()
+            .then(function () {
+              return q.all(tables.map(function (table) {
+                return db.createCollectionAsync(table)
+                  .then(function () {
+                    return db.collectionAsync(table);
+                  })
+                  .then(function (coll) {
+                    coll = Promise.promisifyAll(coll);
+                    return coll.createIndex({ 'exampleIndex': 'text' });
+                  });
+              }));
+            });
+        })
+        .then(mongoClient.closeAsync)
+        .then(done.bind(null, null));
+    });
+
+    it('should get all the tables in the database as an object with a name property', function (done) {
+      mongo.getTables()
+        .then(function (collections) {
+          _.pluck(collections, 'name').sort().should.eql(tables.sort());
+          done();
+        });
+    });
+
+    // After
+    after(dropTestDatabase);
+  });
+
+  xdescribe('createTables', function () {
+
+    it('should create tables passed to it as an object', function (done) {
+      var tables = [
+        { name: 'hello' },
+        { name: 'hello' + Math.random() },
+        { name: 'hello2' },
+      ];
+      mongo.createTables(tables)
+        .then(function () {
+          return mongo.getTables();
+        })
+        .then(function (_tables) {
+          _.pluck(tables, 'name').sort().should.eql(_.pluck(tables, 'name').sort());
+          done();
+        })
+        .catch(done);
+    });
+  });
+
+  xdescribe('getNumberOfRows', function () {
+
+    before(insertTestData);
+
+    it('should get the number of rows in a collection', function (done) {
+      mongo.getNumberOfRows('table1')
+        .then(function (numOfRows) {
+          numOfRows.should.equal(3);
+          done();
+        })
+        .catch(done);
+    });
+
+    after(dropTestDatabase);
+  });
+
+  xdescribe('getRows', function () {
+
+    before(insertTestData);
+
+    it('should get the rows in a collection', function (done) {
+      mongo.getRows('table1', 2, 0)
+        .then(function (rows) {
+          rows.should.be.an.Array;
+          rows.length.should.equal(2);
+          rows[0].number.should.equal(1);
+          done();
+        });
+    });
+
+    after(dropTestDatabase);
+  });
+
+  xdescribe('insertRows', function () {
+
+    before(insertTestData);
+
+    it('should insert the rows into the collection', function (done) {
+      mongo.insertRows('table1', [{ hello: 1 }, { hello: 2 }])
+       .then(function () {
+         return mongo.getRows('table1', 10, 0);
+       })
+       .then(function (rows) {
+         rows.length.should.equal(5);
+         rows.should.containDeep([{ hello: 1 }, { hello : 2 }]);
+         done();
+       });
+    });
+
+    after(dropTestDatabase);
   });
 });
+
